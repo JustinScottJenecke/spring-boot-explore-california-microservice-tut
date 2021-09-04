ca60a0c79714fdb0f85709616d82a6fb67732317
Justin <justinjenecke@gmail.com>
2021-09-04 17:33:04 +0200

    initial commit, creates template web app using spring initializer

---
+++ src/main/java/za/ac/cput/ExploreCaliforniaTut/ExploreCaliforniaTutApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.ExploreCaliforniaTut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliforniaTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliforniaTutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/ac/cput/ExploreCaliforniaTut/ExploreCaliforniaTutApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.ExploreCaliforniaTut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliforniaTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
